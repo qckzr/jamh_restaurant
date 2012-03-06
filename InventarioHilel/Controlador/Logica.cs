2859c5a5ccb22326a9d9f1d3708a9ca548fcb100
Hector Sam <hfsam88@gmail.com>
2012-03-06 17:11:57 -0430

    agregando la clase logica, ya tenemos 2 ventanas y validamos el login de un usuario

---
+++ InventarioHilel/Controlador/Logica.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DAOBaseDatos = InventarioHilel.Modelo.DAOBaseDatos;
+using System.Data;
+
+namespace InventarioHilel.Controlador
+{
+    class Logica
+    {
+        private DAOBaseDatos db;
+
+        private static Logica instancia;
+
+        private Logica()
+        {
+            db = new DAOBaseDatos("Data Source=../../archivos/bd.sqlite3;Version=3");
+        }
+
+        public static Logica getInstance()
+        {
+            if (instancia == null)
+                instancia = new Logica();
+            return instancia;
+        }
+
+        public DAOBaseDatos getDb()
+        {
+            return db;
+        }
+
+        public Boolean validarLogin(String nick, String contrasena)
+        {
+            DataTable a = db.consultar("select * from usuario where nickname ='" + nick + "' and contrasena = '" + contrasena + "'");
+            if (a.Rows.Count> 0)
+                return true;
+            else
+                return false;
+        }
+    }
+}
--- InventarioHilel/Vista/Login.xaml.cs
+++ InventarioHilel/Vista/Login.xaml.cs
@@ -11,6 +11,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Logica = InventarioHilel.Controlador.Logica;
+using System.Data.SQLite;
+using System.Data;
 
 namespace InventarioHilel.Vista
 {
@@ -24,11 +27,25 @@ namespace InventarioHilel.Vista
             InitializeComponent();
         }
 
-        /*private void Window_Loaded(object sender, RoutedEventArgs e)
+        private void buttonIngresar_Click(object sender, RoutedEventArgs e)
         {
-            InventarioHilel.Modelo.DAOBaseDatos a = new Modelo.DAOBaseDatos();
+            if (Logica.getInstance().validarLogin(textBoxUsuario.Text, textBoxContrasena.Text) == true)
+            {
+                Window asd = new MenuOpciones();
+                asd.Visibility = Visibility.Visible;
+                this.Visibility = Visibility.Collapsed;
+            }
+            else 
+                MessageBox.Show("Error en el nick o en la contrasena");
+     
+            
+        }
+/*
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            InventarioHilel.Modelo.DAOBaseDatos a = new InventarioHilel.Modelo.DAOBaseDatos();
             a.extraerInformacion();
         }
-         * */
+  */      
     }
 }
