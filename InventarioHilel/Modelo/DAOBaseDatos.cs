f75e01bed10916df37918c3595c750b8262ec8fd
Hector Sam <hfsam88@gmail.com>
2012-03-06 17:10:56 -0430

    agregando la otra tabla, la de usuario y arreglando lo del dao

--- InventarioHilel/Modelo/DAOBaseDatos.cs
+++ InventarioHilel/Modelo/DAOBaseDatos.cs
@@ -11,13 +11,65 @@ namespace InventarioHilel.Modelo
     {
         //Asi se referencia una carpeta interna =.=, FUCKING visual studio
 
+        private SQLiteConnection conexion;
+
+        public DAOBaseDatos()
+        {
+ 
+        }
+
+        public DAOBaseDatos(String path)
+        {
+            conexion = new SQLiteConnection(path);
+        }
+
+        public void conectar()
+        {
+            conexion.Open();
+        }
+
+        public void desconectar()
+        {
+            conexion.Close();
+        }
+
+        public DataTable consultar(String query)
+        {
+            conectar();
+            SQLiteDataAdapter db = new SQLiteDataAdapter(query, conexion);
+            DataSet ds = new DataSet();
+            DataTable dt = new DataTable();
+
+
+            ds.Reset();
+            db.Fill(ds);
+
+            dt = ds.Tables[0];
+            desconectar();
+            return dt;
+  /*          SQLiteCommand cmd = new SQLiteCommand(query, conexion);
+            SQLiteDataReader datos = cmd.ExecuteReader();
+            // Leemos los datos de forma repetitiva
+            while (datos.Read())
+            {
+                string codigo = Convert.ToString(datos[0]);
+
+                string nombre = Convert.ToString(datos[1]);
+                // Y los mostramos
+
+                Console.WriteLine("Codigo: {0}, Nombre: {1}",
+                    codigo, nombre);
+            }*/
+        }
+
+
         public void extraerInformacion()
         {
             SQLiteConnection conexion = new SQLiteConnection("Data Source=../../archivos/bd.sqlite3;Version=3");
             conexion.Open();
 
             //Se conecta a la BD y trae los datos para, luego, llenar el DataSet
-            string strSQL = "select * from prueba";
+            string strSQL = "select * from usuario";
             SQLiteDataAdapter db = new SQLiteDataAdapter(strSQL, conexion);
             DataSet ds = new DataSet();
             DataTable dt = new DataTable();
