f9aac7c983a64ca1757226b0a6850f6b1fd840eb
jperozo <juanfran_121@hotmail.com>
2012-03-07 23:04:13 -0430

    Archivo de Configuracion y Clase que lo manipula

---
+++ InventarioHilel/Controlador/Configuracion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+
+namespace InventarioHilel.Controlador
+{
+    class Configuracion
+    {
+        private string BDpath;
+
+        public string BD
+        {
+            get { return BDpath; }
+            set { BDpath = value; }
+        }
+
+        public Configuracion()
+        {
+            BDpath = ConfigurationManager.AppSettings["BaseDeDatos"];
+        }
+    }
+}
