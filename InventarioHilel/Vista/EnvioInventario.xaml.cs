baba3be9ac06506b227e2c69ec83f6d2799e2d8d
jperozo <juanfran_121@hotmail.com>
2012-03-09 16:11:31 -0430

    Debug

---
+++ InventarioHilel/Vista/EnvioInventario.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace InventarioHilel.Vista
+{
+    /// <summary>
+    /// Interaction logic for ActualizarInventarioUsuario.xaml
+    /// </summary>
+    public partial class ActualizarInventarioUsuario : Window
+    {
+        public ActualizarInventarioUsuario()
+        {
+            InitializeComponent();
+            WindowState = WindowState.Maximized;
+            WindowStyle = WindowStyle.None;
+
+        }
+    }
+}
