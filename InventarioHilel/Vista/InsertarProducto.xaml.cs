9b76e89ee100209456884065d1e37710437d89e0
aileen <aileen.posadas@gmail.com>
2012-03-12 11:39:36 -0430

    vistas de productos y de usuario

---
+++ InventarioHilel/Vista/InsertarProducto.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace InventarioHilel.Vista
+{
+    /// <summary>
+    /// Interaction logic for InsertarProducto.xaml
+    /// </summary>
+    public partial class InsertarProducto : Window
+    {
+        public InsertarProducto()
+        {
+            InitializeComponent();
+            WindowState = WindowState.Maximized;
+            WindowStyle = WindowStyle.None;
+
+
+        }
+
+        private void b_cerrarAdmin_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
